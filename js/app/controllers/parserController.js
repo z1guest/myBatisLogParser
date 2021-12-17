792574b27bc5a9819e973d5ce32dfa2f47d520b6
z1guest <73040542+z1guest@users.noreply.github.com>
2021-12-17 11:34:57 +0900

    Update parserController.js

--- js/app/controllers/parserController.js
+++ js/app/controllers/parserController.js
@@ -60,7 +60,7 @@ tipJS.controller({
 
 			if ( preparing.indexOf("?") >= 0 ) {
 
-				if (param.typ === "String" || param.typ === "Date") {
+				if (param.typ === "String" || param.typ === "Date" || param.typ === "Timestamp") {
 					param.val = "'"+param.val+"'";
 				}
 				preparing = preparing.replace("?", param.val);
